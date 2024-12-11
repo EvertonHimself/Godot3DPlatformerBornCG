5490298b6b25719fff516023eef9e603a6f16d97
EvertonHimself <evertondeoliveira@gmail.com>
2024-12-11 18:00:16 -0300

    Update icon.gd
    Created some testing variables.

--- 3d-platformer-game/icon.gd
+++ 3d-platformer-game/icon.gd
@@ -1,11 +1,15 @@
 extends Sprite3D
 
+var coins = 5
+var player_name = "robot"
+var hearts = 3.5
+const SPEED = 2
+var key_collected
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	rotate_y(deg_to_rad(SPEED))
